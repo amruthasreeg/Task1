38899a5bd2844bb864ad600f99a591292f235fcc
amruthasreeg <amruthasreeg1993@gmail.com>
2021-04-09 13:10:03 +0530

    Task Done

---
+++ src/org/test/GitClass1.java
@@ -0,0 +1,26 @@
+package org.test;
+
+public class GitClass1 
+{
+ public void demo1()
+ {
+	 System.out.println("demo 1");
+ }
+ 
+ public void demo2()
+ {
+	 System.out.println("demo 2");
+ }
+ 
+ public void demo3()
+ {
+	 System.out.println("demo 3");
+ }
+ public static void main(String args[])
+ {
+	 GitClass1 gc=new GitClass1();
+	 gc.demo1();
+	 gc.demo2();
+	 gc.demo3();
+ }
+}
