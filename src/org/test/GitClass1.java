463239df0be2baf924f98fde4b5e8185e85e36b8
gayathi694 <gaya63672@gmail.com>
2021-04-09 13:52:50 +0530

    Brach1 Done

--- src/org/test/GitClass1.java
+++ src/org/test/GitClass1.java
@@ -16,11 +16,29 @@ public class GitClass1
  {
 	 System.out.println("demo 3");
  }
+ 
+ public void demo4()
+ {
+	 System.out.println("demo 4");
+ }
+ 
+ public void demo5()
+ {
+	 System.out.println("demo 5");
+ }
+ 
+ public void demo6()
+ {
+	 System.out.println("demo 6");
+ }
  public static void main(String args[])
  {
 	 GitClass1 gc=new GitClass1();
 	 gc.demo1();
 	 gc.demo2();
 	 gc.demo3();
+	 gc.demo4();
+	 gc.demo5();
+	 gc.demo6();
  }
 }
