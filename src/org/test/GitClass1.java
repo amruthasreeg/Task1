eb7b0d67a4e4b2c4e4525f71fee30c8989aa6f62
ShinyBPrincy <shinyprincy.jbm@gmail.com>
2021-04-10 01:28:50 +0530

    demo done

--- src/org/test/GitClass1.java
+++ src/org/test/GitClass1.java
@@ -16,11 +16,26 @@ public class GitClass1
  {
 	 System.out.println("demo 3");
  }
+ public void demo7()
+ {
+	 System.out.println("demo 7");
+ }
+ public void demo8()
+ {
+	 System.out.println("demo 8");
+ }
+ public void demo9()
+ {
+	 System.out.println("demo 9");
+ }
  public static void main(String args[])
  {
 	 GitClass1 gc=new GitClass1();
 	 gc.demo1();
 	 gc.demo2();
 	 gc.demo3();
+	 gc.demo7();
+	 gc.demo8();
+	 gc.demo9();
  }
 }
